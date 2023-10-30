887369f545d6f96cba6ab40cc674caae78605fb9
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-10-30 20:55:20 +0530

    Add files via upload

---
+++ List/List Methods/Index Method.py
@@ -0,0 +1,4 @@
+# index():- Returns the lowest index where the element is appears.
+list=[1,3, 5, 7, 9]
+n=int(input("Enter the element whose index you want to return: "))
+print("The element is at: ", list.index(n))
