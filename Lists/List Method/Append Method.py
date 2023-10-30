8c16a8021077592b5a2f5459b3fb233178425f8d
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-10-30 20:09:07 +0530

    Delete Lists/List Method directory

--- Lists/List Method/Append Method.py
+++
@@ -1,7 +0,0 @@
-# append():- This methods add an element at the end of the list.
-List=[]
-n=int(input("Enter the number of list items required: "))
-for i in range(0, n):
-    elements=int(input(f"Enter the list item {i}: "))
-    List.append(elements)
-print(List)
