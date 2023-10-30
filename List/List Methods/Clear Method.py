91abd0eff6c964703f86caf5b8ffdf9df998abf8
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-10-30 20:36:31 +0530

    Add files via upload

---
+++ List/List Methods/Clear Method.py
@@ -0,0 +1,5 @@
+# clear():- It removes all the elements in the list.
+list=[2, 4, 6, 8, 10]
+print(f"Before clearing the list: {list}.")
+list.clear()
+print(f"After clearing the list: {list}.")
---
+++ List/List Methods/Copy Method.py
@@ -0,0 +1,5 @@
+# copy():- It returns the shallow copy of a list.
+list1=[11, 2, 3, 4]
+list2=[]
+list2=list1.copy()
+print(f"These list is the copied from list1 or the original list: {list2}")
