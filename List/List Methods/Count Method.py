400385893c96af8abd9cc559f36f3a8ed87c0c29
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-10-30 20:43:42 +0530

    Update Count Method.py

--- List/List Methods/Count Method.py
+++ List/List Methods/Count Method.py
@@ -1,3 +1,3 @@
-# count():- This methods count the elements.
+# count():- This methods count the specified elements. Here we have specified 2.
 list=[2, 2, 2, 1, 2, 3, 4, 5]
 print(f"{list.count(2)}.")
