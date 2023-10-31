d09466f5c1a7202e39736f827acb5ac4664a91a7
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-10-31 20:25:19 +0530

    Add files via upload

---
+++ Tuple/Tuple Methods/Count Method.py
@@ -0,0 +1,4 @@
+# count():- Returns the number of times a specified value occurs in a tuple.
+tuple=(1, 2, 2, 3, 2, 4, 2, 4)
+n=int(input("Enter the element which you want count: "))
+print(f"There are {tuple.count(n)}, {n}'s in the list.")
