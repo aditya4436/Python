0a3977b87c07c939a406671744a8061d4ae28c46
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-10-31 20:16:33 +0530

    Delete List/Tuple/Tuple.py

--- List/Tuple/Tuple.py
+++
@@ -1,8 +0,0 @@
-# Tuple:- It is a collection of objects separated by commas. These are immutable.
-tuple=(2, 11, 29, 1)
-print("The elements in the tuple are: ", tuple)
-
-t=(1, 2, 3, 4)
-t1=(5, 6, 7, 8)
-t2=(t, t1)
-print("This is the nested tuple: ", t2)
