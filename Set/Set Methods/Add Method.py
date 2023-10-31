cfd44996eb384964cda418b301db0beee217df07
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-10-31 21:09:44 +0530

    Add files via upload

---
+++ Set/Set Methods/Add Method.py
@@ -0,0 +1,6 @@
+# add():- Adds a given element to a set.
+set={64, 41, 99, 80}
+print("The elements in the set are: ", set)
+n=int(int(input("Enter the number you wish to add: ")))
+set.add(n)
+print(f"The set element after inserting an element: {set}")
