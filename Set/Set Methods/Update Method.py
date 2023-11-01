aa321d5562b28e6ff1d82ce8b6ec73c6ee698895
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-01 21:20:23 +0530

    Add files via upload

---
+++ Set/Set Methods/Update Method.py
@@ -0,0 +1,7 @@
+# update():- Update's the set with another set or any other iterable.
+set1={11, 2, 18, 108}
+set2={34, 11, 108, 21}
+print(f"The first set is: {set1}")
+print(f"The second set is: {set2}")
+set1.update(set2)
+print(f"Set1 is updated with set1 and set2 is: {set1}")
