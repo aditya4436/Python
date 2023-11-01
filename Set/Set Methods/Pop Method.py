74d9de688690a7cd6f11e095b716fc4595feb879
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-01 20:52:17 +0530

    Add files via upload

---
+++ Set/Set Methods/Pop Method.py
@@ -0,0 +1,5 @@
+# pop():- Removes an element from the set.
+set1={11, 18, 2, 108}
+print(f"The first set is: {set1}")
+set1.pop()
+print(f"The set after popping an element: {set1}")
---
+++ Set/Set Methods/Remove Method.py
@@ -0,0 +1,6 @@
+# removes():- Removes the specified element from the set.
+set1={11, 18, 2, 108}
+print(f"The first set is: {set1}")
+n=int(input("Enter the item that you wish to remove: "))
+set1.remove(n)
+print(f"The set after removing an element: {set1}")
