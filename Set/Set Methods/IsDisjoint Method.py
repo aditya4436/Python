c6a4fa03c088bcf27be21e6944a965944595a260
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-01 20:35:54 +0530

    Add files via upload

---
+++ Set/Set Methods/IsDisjoint Method.py
@@ -0,0 +1,6 @@
+# isdisjoint():- Returns whether two sets have a intersection or not.
+set1={11, 2, 18, 108}
+set2={21, 3, 81, 801}
+print(f"The first set is: {set1}")
+print(f"The second set is: {set2}")
+print(f"Set1 and set2 are disjoint set: {set1.isdisjoint(set2)}")
