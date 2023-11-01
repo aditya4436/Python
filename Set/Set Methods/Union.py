70dd878484d6f9133838d3bf35b6add4ec4eacd6
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-01 21:12:15 +0530

    Add files via upload

---
+++ Set/Set Methods/Union.py
@@ -0,0 +1,7 @@
+# union():- Returns a set containing the union of sets.
+set1={11, 2, 18, 108}
+set2={34, 11, 108, 21}
+print(f"The first set is: {set1}")
+print(f"The second set is: {set2}")
+set3=set1.union(set2)
+print(f"The union of the two set is: {set3}")
