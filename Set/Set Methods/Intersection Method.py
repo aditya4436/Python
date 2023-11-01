fcbd1cb03b411e49d4b77afe49d02ef9d462964a
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-01 20:20:48 +0530

    Add files via upload

---
+++ Set/Set Methods/Intersection Method.py
@@ -0,0 +1,6 @@
+set1={21, 34, 54, 78, 90}
+set2={21, 90, 24, 33, 108}
+print(f"The first set is: {set1}")
+print(f"The second set is: {set2}")
+set3=set1.intersection(set2)
+print(f"The intersection of the two given sets is: {set3}")
