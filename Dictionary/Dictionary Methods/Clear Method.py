ddb91211b87a708cc876868a1d34daf18651a4c6
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-02 20:08:41 +0530

    Add files via upload

---
+++ Dictionary/Dictionary Methods/Clear Method.py
@@ -0,0 +1,5 @@
+# clear():- Removes all the elements from the dictionary.
+dictionary={1: "Aditya", 2: "Kumar",  3: "Pawan"}
+print("The elements in the dictionary are: ", dictionary)
+dictionary.clear()
+print(dictionary)
