a653c135173f60710d4e1565741752f27f1feb34
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-02 20:46:26 +0530

    Add files via upload

---
+++ Dictionary/Dictionary Methods/Items Method.py
@@ -0,0 +1,6 @@
+# items():- Returns a list containing a tuple for each key value pair.
+#           It basically means it encapsulates key-value pair int tuple
+#           and again the tuples are encapsulated into list.
+dictionary={1: "Software", 2: "Development", 3: "Competitive", 4: "Programming"}
+print("The dictionary items: ", dictionary)
+print("Returns a list containing a tuple for each key value pair: ", dictionary.items())
