1c446f94fd79fac6ee282f2888bf7315ce144b81
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-02 20:00:52 +0530

    Add files via upload

---
+++ Dictionary/Dictionary.py
@@ -0,0 +1,12 @@
+# Dictionary:- In Python it is a collection of keys values, used to 
+#              to store data values like a map, which, unlike other 
+#              data types which hold only single value as an element.
+
+# Dictionary holds "key:value" pair. Key-Value is provided in the dictionary
+# to make it more optimized.
+dictionary={1: "Aditya", 2: "Kumar",  3: "Pawan"}
+x=dictionary.keys()
+y=dictionary.values()
+print(f"The elements in the dictionary are: {dictionary}")
+print(f"The keys of the element int the dictionary are: {x}")
+print(f"The values of the element int the dictionary are: {y}")
