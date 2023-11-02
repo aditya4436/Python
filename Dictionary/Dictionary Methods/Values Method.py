4cd6ee89c25bd3d52701ec68e4065487fb217c01
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-02 21:28:52 +0530

    Add files via upload

---
+++ Dictionary/Dictionary Methods/Values Method.py
@@ -0,0 +1,4 @@
+# values():- Returns a list of all the values in the dictionary.
+dictionary={1: "Data", 2: "Structure", 3: "And", 4: "Algorithm"}
+print("The dictionary is: ", dictionary)
+print("The list of all the values in the dictionary: ", dictionary.values())
