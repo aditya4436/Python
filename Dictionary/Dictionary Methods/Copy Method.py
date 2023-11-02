315bf4ac1f8c36b378e60748ef3535c6dd3244a5
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-02 20:13:26 +0530

    Add files via upload

---
+++ Dictionary/Dictionary Methods/Copy Method.py
@@ -0,0 +1,5 @@
+# copy():- Returns a copy of the dictionary.
+dictionary={1: "Aditya", 2: "Kumar",  3: "Pawan"}
+dictionary1=dictionary.copy()
+print(f"The original dictionary: {dictionary}")
+print(f"The copied dictionary: {dictionary1}")
