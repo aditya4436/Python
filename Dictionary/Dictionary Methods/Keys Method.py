f1d827ac13a33f8c347d63644b26b3adfd5fbbb7
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-02 20:50:58 +0530

    Add files via upload

---
+++ Dictionary/Dictionary Methods/Keys Method.py
@@ -0,0 +1,4 @@
+# keys():- Returns a list containing the dictionary's keys.
+dictionary={1: "Software", 2: "Development", 3: "Competitive", 4: "Programming"}
+print("The items in the dictionary are: ", dictionary)
+print("The list of the keys of the dictionary: ", dictionary.keys())
