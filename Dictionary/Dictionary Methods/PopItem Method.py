d48a76c33bedfa1c636c562d639d9ffaa2dde87f
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-02 21:02:37 +0530

    Add files via upload

---
+++ Dictionary/Dictionary Methods/PopItem Method.py
@@ -0,0 +1,5 @@
+# popitem():- Removes the last inserted key-value pair.
+dictionary={1: "Data", 2: "Structure", 3: "And", 4: "Algorithm"}
+print(f"The dictionary before popping out the last item: {dictionary}")
+dictionary.popitem()
+print(f"The dictionary sfter popping out the last item: {dictionary}")
