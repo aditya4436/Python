45415f769d4b57c563dd9ed95f3e9f788526b880
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-03 20:54:44 +0530

    Add files via upload

---
+++ String/String Methods/Endswith Method.py
@@ -0,0 +1,5 @@
+# endswith():- Returns true if the sting ends with specified value character.
+string=input("Enter any string: ")
+print("The string is: ", string)
+char=input("Enter the character to check if the string ends with it or not: ")
+print(f"The string endswith. {string.endswith(char)}.")
