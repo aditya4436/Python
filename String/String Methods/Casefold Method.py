74f2f4d104b349014b6a3946cd7d78cb297b43c8
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-03 20:20:02 +0530

    Add files via upload

---
+++ String/String Methods/Casefold Method.py
@@ -0,0 +1,4 @@
+# casefold():- Converts the string into lowercase.
+string=input("Enter any string: ")
+print(f"The entered string is: {string}")
+print(f"After converting the string into lower case: {string.casefold()}")
