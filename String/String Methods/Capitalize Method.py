b5cdebbf1db191d3694da369453aa0f96974f161
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-03 20:15:39 +0530

    Add files via upload

---
+++ String/String Methods/Capitalize Method.py
@@ -0,0 +1,4 @@
+# capitalize():- Converts the first character to upper case.
+string=input("Enter any string: ")
+print(f"The string you have entered is: {string}")
+print(f"The string after capitailzation: {string.capitalize()}")
