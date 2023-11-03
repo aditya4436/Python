9cbfaec175322a9e1f177ee16fa78a264ead868a
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-03 20:09:43 +0530

    Add files via upload

---
+++ String/String.py
@@ -0,0 +1,9 @@
+# Strings:-Strings in Python are surrounded by either single quotation
+#          marks, or double quotation marks. We can assign multiline
+#          to variable using triple (single or double) quotation marks.
+#          These are also sometimes known as Doctstring in Python.
+print("Hello", 'World')
+a="""We can assign multiline
+     to variable using triple (single or double) quotation marks.
+     These are also sometimes known as Doctstring in Python. """
+print(a)
