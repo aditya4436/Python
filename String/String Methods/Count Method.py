c290c8a9bc57ac1cbb49ff7a3bc07b0304aa38e3
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-03 20:32:50 +0530

    Add files via upload

---
+++ String/String Methods/Count Method.py
@@ -0,0 +1,6 @@
+# count():- Returns the number of times a specified value or character
+#           occurs in a string.
+string=input("Enter any string: ")
+print("The string is: ", string)
+string1=input("Enter the character in the string that you want to print the count of: ")
+print(f"The character {string1} is repeated {string.count(string1)}.")
