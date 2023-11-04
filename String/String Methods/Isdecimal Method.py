ac3c3ce1cf3d7c9b4d67783e7cb727fa3ec88f28
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-04 20:38:34 +0530

    Add files via upload

---
+++ String/String Methods/Isdecimal Method.py
@@ -0,0 +1,5 @@
+# isdecimal():- This method returns True if all the characters in the string are
+#               decimal(0-9).
+string=input("Enter a string: ")
+print(f"The string is: {string}.")
+print(f"The entered string is a decimal: {string.isdecimal()}")
