74deffb3a92817f8dd2ac5d84961fa17a3a92810
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-04 20:32:40 +0530

    Add files via upload

---
+++ String/String Methods/IsASCII Method.py
@@ -0,0 +1,5 @@
+# isascii():- Returns True if all the characters in the string are ascii characters.
+# string=input("Enter a string: ")
+string="  "
+print(f"The string is: {string}.")
+print(string.isascii())
