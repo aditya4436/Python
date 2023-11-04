41520bcee3a44e4a34e548c311bfb7ba2cf37997
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-04 20:46:23 +0530

    Add files via upload

---
+++ String/String Methods/Isdigit Method.py
@@ -0,0 +1,4 @@
+# isdigit():- Returns True if all the characters in the string are digits.
+string=input("Enter a string: ")
+print(f"The string is: {string}.")
+print(f"All the character in the string are digits: {string.isdigit()}")
