0260781499cf42b7db41119c855a6293f0227fc4
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-04 20:23:21 +0530

    Add files via upload

---
+++ String/String Methods/Isalpha Method.py
@@ -0,0 +1,5 @@
+# isalpha():- Returns true if all the charcters in the string are
+#             alphabet.
+string=input("Enter a string: ")
+print(f"The string is: {string}.")
+print(f"The entered is alpha(alpahabet): {string.isalpha()}")
