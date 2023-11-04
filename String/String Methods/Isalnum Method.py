16f31666db232887bede0656c37be76a051c9ce9
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-04 20:19:30 +0530

    Add files via upload

---
+++ String/String Methods/Isalnum Method.py
@@ -0,0 +1,5 @@
+# isalnum():- Returns True if all the characters in the string is alphanumeric
+#             or either numeric or alpha.
+string=input("Enter a string: ")
+print(f"The string is: {string}.")
+print("The entered string is alphanumeric: ", string.isalnum())
