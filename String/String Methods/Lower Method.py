440bc7afb162ce304891ef1bb6a24a9083eed5c1
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:44:44 +0530

    Add files via upload

---
+++ String/String Methods/Lower Method.py
@@ -0,0 +1,4 @@
+# lower():- It converts all the character of the string into lowercase.
+string=input("Enter the string: ")
+print(f"The entered string is: {string}")
+print(f"After converting each character of the string into lower case: {string.lower()}")
