7fcfa3fc7b37728c448d43ae6622a7706a9bb82b
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:19:47 +0530

    Add files via upload

---
+++ String/String Methods/Istitle Method.py
@@ -0,0 +1,11 @@
+# istitle():- Returns True if all words in a text start with a upper case
+#             letter, and rest of the word are lower case letters, otherwise
+#             false.
+string="PYTHON"
+string1="Python Programming Language"
+string2="123 Aditya"
+string3="This is @ Python ! String"
+print(string.istitle())
+print(string1.istitle())
+print(string2.istitle())
+print(string3.istitle())
