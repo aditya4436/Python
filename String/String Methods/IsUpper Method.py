68bbbbbb993c2487cd58fe03d8332e0f7f347625
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:24:36 +0530

    Add files via upload

---
+++ String/String Methods/IsUpper Method.py
@@ -0,0 +1,4 @@
+# isupper():- Returns True if all the characters of the string is in upper case.
+string=input("Enter the string: ")
+print(f"The entered string is: {string}")
+print(f"The string is: {string.isupper()}")
