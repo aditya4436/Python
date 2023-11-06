4a3eb167252ff34e7f5af0e20ee0e53c0738003d
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 19:54:13 +0530

    Add files via upload

---
+++ String/String Methods/Islower Method.py
@@ -0,0 +1,5 @@
+# islower():- Returns True if all the characters int the string are lower
+#             case.
+string=input("Enter the string: ")
+print("The entered string is :", string)
+print(f"The characters of the string are in lower case: {string.islower()}")
