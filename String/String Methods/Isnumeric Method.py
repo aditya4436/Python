06047b6ce5175dedfda6c945f0bcd39ae4ada6c9
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:00:22 +0530

    Add files via upload

---
+++ String/String Methods/Isnumeric Method.py
@@ -0,0 +1,4 @@
+string=input("Enter the string: ")
+print("The entered string is :", string)
+# string="\u0030" The output will be true.
+print("The entered string is numeric :", string.isnumeric())
