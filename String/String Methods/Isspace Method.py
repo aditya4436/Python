8ea6a31775b1d63aebd15e04574bd05334b63874
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:13:15 +0530

    Add files via upload

---
+++ String/String Methods/Isspace Method.py
@@ -0,0 +1,6 @@
+# isspace():- Returns True if the character in the string is/are 
+#             whitespace.
+string="  "
+string1="Aditya"
+print("The first string is a whitesapce: ", string.isspace())
+print("The second string is a whitesapce: ", string1.isspace())
