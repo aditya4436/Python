887d5a88503f1c742a85d1849c1487a5cffe0b67
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:40:58 +0530

    Add files via upload

---
+++ String/String Methods/Ljust Method.py
@@ -0,0 +1,4 @@
+# ljust():- This method will left align the string, using a specified character
+#           (space is default) as the fill character.
+string="Google"
+print(f"{string.ljust(10)} is one of the best product based IT company.")
