3494bfbb336c4ecbfa07dd9682052aec46f6545c
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:02:48 +0530

    Update Isnumeric Method.py

--- String/String Methods/Isnumeric Method.py
+++ String/String Methods/Isnumeric Method.py
@@ -1,3 +1,4 @@
+# isnumeric():- Returns True if all the characters in the string are numeric.
 string=input("Enter the string: ")
 print("The entered string is :", string)
 # string="\u0030" The output will be true.
