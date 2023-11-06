9d9872f37cc43ec066b86229c6f7527fff313051
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:08:01 +0530

    Add files via upload

---
+++ String/String Methods/Isprintable Method.py
@@ -0,0 +1,6 @@
+# isprintable():- Returns True if all the characters are printable
+#                 otherwise False.
+string="\n"
+string1="Aditya"
+print(f"The string is printable: {string.isprintable()}")
+print(f"The string is printable: {string1.isprintable()}")
