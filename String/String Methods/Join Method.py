b21c193eaaae28a735333823594895ccb87bf65e
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 20:35:05 +0530

    Add files via upload

---
+++ String/String Methods/Join Method.py
@@ -0,0 +1,6 @@
+# join():- It takes all the items in an iterable and joins them into
+#          one string.
+list=["name", "Aditya", "branch", "CSE"]
+mySeparator="-"
+print(f"The elements in the list are: {list}")
+print(f"The string is: {mySeparator.join(list)}")
