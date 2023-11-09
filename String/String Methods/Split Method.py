cfea2cab89b7c9b528fdabb94b850a0b610a04ba
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-09 20:16:32 +0530

    Add files via upload

---
+++ String/String Methods/Split Method.py
@@ -0,0 +1,5 @@
+# split():- Splits the string at the specified separator, and
+#           returns a list.
+string=input("Enter the string: ")
+print("The entered string is: ", string)
+print(f"The string after splitting: {string.split()}.")
