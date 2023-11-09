f1184899e4826445e51f23b13a54888f6306cc8f
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-09 20:22:25 +0530

    Add files via upload

---
+++ String/String Methods/SplitLines Method.py
@@ -0,0 +1,4 @@
+# splitline():- This method splits a string into a list. The spliting is
+#               is done at line  breaks.
+string="Spliting the string.\n On the basis of the lines."
+print(f"The string after splitting: {string.splitlines()}")
