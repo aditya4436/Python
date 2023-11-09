0a6f7be64e5f3a4b58d2b5150f7eaa47277c4d00
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-09 20:40:42 +0530

    Add files via upload

---
+++ String/String Methods/Strip Method.py
@@ -0,0 +1,4 @@
+# strip():- Removes the any leading and trailing  whitespaces.
+string="     Programming      "
+print(f"The string is: {string}")
+print(f"The string after removing the leading and trailing whitespaces: {string.strip()}")
