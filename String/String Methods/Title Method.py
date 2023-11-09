061b62711149e7e39062eaf8ad003945c3554f09
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-09 20:52:06 +0530

    Add files via upload

---
+++ String/String Methods/Title Method.py
@@ -0,0 +1,4 @@
+# title():- It converts the first character of each word into uppercase.
+string=input("Enter the string: ")
+print(f"The string is: {string}")
+print(f"The string after converting the first letter of each word to uppercase: {string.title()}")
