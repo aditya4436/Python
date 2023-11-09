f44cf5bd7b6b0d2c18721c295a01934eae3349c0
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-09 20:12:50 +0530

    Add files via upload

---
+++ String/String Methods/RIndex Method.py
@@ -0,0 +1,6 @@
+# rindex():- This method finds the last occurence of the specified value.
+string=input("Enter the string: ")
+string1=input("Enter the string which you want to find: ")
+print(f"The string is: {string}")
+print(f"The string which is to be searched: {string1}")
+print(f"The string starts from the index: {string.rindex(string1)}.")
