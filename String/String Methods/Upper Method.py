b706c01120879d50592aae9f3ecebb2bb7e831b9
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-09 21:04:35 +0530

    Add files via upload

---
+++ String/String Methods/Upper Method.py
@@ -0,0 +1,4 @@
+# upper():- Converts the string into upper case.
+string=input("Enter the string: ")
+print(f"The entered string is: {string}.")
+print(f"After converting the string to the uppercase: {string.upper()}")
