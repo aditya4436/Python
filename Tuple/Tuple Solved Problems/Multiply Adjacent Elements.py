fffd7cf1685e37f6732adb40d83382474cd75008
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-18 20:53:27 +0530

    Update Multiply Adjacent Elements.py

--- Tuple/Tuple Solved Problems/Multiply Adjacent Elements.py
+++ Tuple/Tuple Solved Problems/Multiply Adjacent Elements.py
@@ -1,5 +1,8 @@
 # Time Complexity:- O(n)
 # Space Complexity:- O(n)
 tup1=(1, 5, 7, 8, 10)
-result=tuple(i*j for i, j in zip(tup1, tup1[1:]))
-print(result)
+# result=tuple(i*j for i, j in zip(tup1, tup1[1:]))
+# print(result)
+
+result=tuple(map(lambda i, j: i*j, tup1[1:], tup1[:-1]))
+print(f"The result is: {result}.")
